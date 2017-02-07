9885494b70113cf5588c3ec2e23a1afc3f08bdaf
Jay Ginete <Jay Ginete>
2017-02-07 16:18:30 +0800

    added freemaker template

--- src/main/java/com/sample/sparkdemo/DemoMain.java
+++ src/main/java/com/sample/sparkdemo/DemoMain.java
@@ -1,12 +1,26 @@
 package com.sample.sparkdemo;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;
 import static spark.Spark.*;
+
 /**
  *
  * @author jaypax
  */
 public class DemoMain {
+
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World!");
+        staticFiles.location("/css"); // Static files
+        
+        get("/hello", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("title","SparkJava Demo");
+            model.put("message", "Hello Freemarker!");
+            return new ModelAndView(model, "hello.ftl"); // located in src/test/resources/spark/template/freemarker
+        }, new FreeMarkerEngine());
     }
 }
